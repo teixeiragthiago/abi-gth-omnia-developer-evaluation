6cc669bfcb0e9805e7433b03a4bc26f311b3f259
teixeiragthiago <kbeca-thiago@hotmail.com>
2026-01-24 01:20:45 -0300

    feat(domain): add entities and ef configs

---
+++ src/Ambev.DeveloperEvaluation.ORM/Mapping/SaleConfiguration.cs
@@ -0,0 +1,54 @@
+using Ambev.DeveloperEvaluation.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Ambev.DeveloperEvaluation.ORM.Mapping;
+
+public class SaleConfiguration
+{
+    public void Configure(EntityTypeBuilder<Sale> builder)
+    {
+        builder.ToTable(nameof(Sale));
+
+        builder.HasKey(x => x.Id);
+        builder.Property(u => u.Id)
+            .HasColumnType("uuid")
+            .HasDefaultValueSql("gen_random_uuid()");
+
+        builder.Property(s => s.SaleNumber)
+            .UseIdentityAlwaysColumn()
+            .IsRequired();
+
+        builder.Property(s => s.CreatedAt)
+            .IsRequired()
+            .HasDefaultValueSql("timezone('utc', now())");
+        
+        builder.Property(s => s.UpdatedAt);
+        builder.Property(s => s.CancelledAt);
+
+        builder.Property(s => s.CustomerId)
+            .HasColumnType("uuid")
+            .IsRequired();
+
+        builder.Property(s => s.BranchId)
+            .HasColumnType("uuid")
+            .IsRequired();
+
+        builder.Property(s => s.TotalAmount)
+            .HasColumnType("numeric(18,2)")
+            .IsRequired();
+
+        builder.Property(s => s.IsCancelled)
+            .IsRequired();
+
+        builder.HasMany(s => s.Items)
+            .WithOne()
+            .HasForeignKey("SaleId")  
+            .OnDelete(DeleteBehavior.Cascade);
+
+        builder.Navigation(s => s.Items)
+            .HasField("_items")
+            .UsePropertyAccessMode(PropertyAccessMode.Field);
+        
+    }
+}
---
+++ src/Ambev.DeveloperEvaluation.ORM/Mapping/SaleItemConfiguration.cs
@@ -0,0 +1,52 @@
+using Ambev.DeveloperEvaluation.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Ambev.DeveloperEvaluation.ORM.Mapping;
+
+public class SaleItemConfiguration
+{
+    public void Configure(EntityTypeBuilder<SaleItem> builder)
+    {
+        builder.ToTable(nameof(SaleItem));
+
+        builder.HasKey(i => i.Id);
+
+        builder.Property(u => u.Id)
+            .HasColumnType("uuid")
+            .HasDefaultValueSql("gen_random_uuid()");
+
+        builder.Property(i => i.SaleId)
+            .HasColumnType("uuid")
+            .IsRequired();
+
+        builder.Property(i => i.ProductId)
+            .HasColumnType("uuid")
+            .IsRequired();
+
+        builder.Property(i => i.Quantity)
+            .IsRequired();
+        builder.Property(i => i.UnitPrice)
+            .HasColumnType("numeric(18,2)")
+            .IsRequired();
+        builder.Property(i => i.DiscountPercentage)
+            .HasColumnType("numeric(5,4)")
+            .IsRequired();
+        builder.Property(i => i.DiscountAmount)
+            .HasColumnType("numeric(18,2)")
+            .IsRequired();
+        builder.Property(i => i.TotalAmount)
+            .HasColumnType("numeric(18,2)")
+            .IsRequired();
+
+        builder.Property(s => s.CreatedAt)
+            .IsRequired()
+            .HasDefaultValueSql("timezone('utc', now())");
+        
+        builder.Property(s => s.UpdatedAt);
+
+        builder.HasOne<Sale>()
+            .WithMany(s => s.Items)
+            .HasForeignKey(i => i.SaleId);
+    }
+}
